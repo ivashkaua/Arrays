be3e13b9cd04b6c9b57caeff9fe208109ca3b6dc
ivashkaua <86633081+ivashkaua@users.noreply.github.com>
2021-09-22 19:39:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов массива: \t");
+            int ArrNumber = int.Parse(Console.ReadLine());
+            int[] array = new int[ArrNumber];
+
+            for (int l = 0; l < ArrNumber; l++)
+            {
+                Console.Write($"\nВведите число с индексом {l} \t");
+                array[l] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Ваш массив выглядит следующим образом: ");
+            for (int i = 0; i < ArrNumber; i++)
+            {
+                Console.WriteLine("\n"+ array[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
